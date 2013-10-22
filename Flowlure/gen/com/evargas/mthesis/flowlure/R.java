e533c2248c962c91aadc4d84636f945b28c1f777
Eric Vargas <eric.vargas.mx@gmail.com>
2013-10-22 16:22:12 -0500

    Components added to Login.
    Atm app is only available for landscape usage. Users should login to the
    app through the main activity.
    
    * Added user field to login.
    * Added a gradient background to login.
    * Landscape mode only active.

--- Flowlure/gen/com/evargas/mthesis/flowlure/R.java
+++ Flowlure/gen/com/evargas/mthesis/flowlure/R.java
@@ -12,9 +12,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int rect_gradient_blue=0x7f020001;
+    }
+    public static final class id {
+        public static final int login_input_user=0x7f060000;
+    }
+    public static final class layout {
+        public static final int login=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int input_password=0x7f040002;
+        public static final int input_username=0x7f040001;
     }
     public static final class style {
         /** 
@@ -38,10 +47,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- Flowlure/src/com/evargas/mthesis/flowlure/LoginActivity.java
+++ Flowlure/src/com/evargas/mthesis/flowlure/LoginActivity.java
@@ -14,6 +14,6 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		
+		setContentView(R.layout.login);
 	}
 }
